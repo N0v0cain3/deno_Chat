571011e55c182d54c14e9a513bf1dec36f83c850
Shivam Mehta <shivam.mehta2019@vitstudent.ac.in>
2020-07-01 10:38:43 +0530

    final

---
+++ README.md
@@ -0,0 +1,20 @@
+# Real-time Chat App with Deno and Websockets
+
+## Demo
+
+#### Check [the following link](https://deno-websocket-chat.herokuapp.com/chat.html) to see deployed version on heroku
+
+--------------------
+
+## Installation
+You need to have [Deno installed](https://deno.land/#installation) in order to run this app locally
+
+1. Clone the repository
+2. Go to the project root using terminal
+3. Run `deno run --allow-net --allow-read server.js`
+4. Open http://localhost:3000/chat.html` in browser
+5. That's all.
+
+
+> The project was created along with Youtube Video ["Build Realtime Chat App with Deno and WebSockets"](https://youtu.be/XWyUtYL6ynE). 
+> I appreaciate if you like the video and share it.
---
+++ server.js
@@ -0,0 +1,58 @@
+//@ts-nocheck
+
+import { listenAndServe } from "https://deno.land/std@0.58.0/http/server.ts";
+import { serveFile } from "https://deno.land/std@0.58.0/http/file_server.ts";
+import {
+  acceptWebSocket,
+  acceptable,
+} from "https://deno.land/std@0.58.0/ws/mod.ts";
+import { parse } from "https://deno.land/std@0.59.0/flags/mod.ts";
+import chat from "./chat.js";
+
+async function fileExists(path) {
+  try {
+    const stats = await Deno.lstat(path);
+    return stats && stats.isFile;
+  } catch (e) {
+    if (e && e instanceof Deno.errors.NotFound) {
+      return false;
+    } else {
+      throw e;
+    }
+  }
+}
+
+const DEFAULT_PORT = 3000;
+const argPort = parse(Deno.args).port;
+const port = argPort ? parseInt(argPort) : DEFAULT_PORT;
+
+listenAndServe({ port: port }, async (req) => {
+  let url = req.url;
+  if (req.method === "GET" && url === "/") {
+    url = "/index.html";
+  }
+
+  const position = req.url.indexOf("?");
+
+  if (position > -1) {
+    url = url.substring(0, position);
+  }
+  const path = `${Deno.cwd()}/public${url}`; // /index.html
+  if (await fileExists(path)) {
+    const content = await serveFile(req, path);
+    req.respond(content);
+    return;
+  }
+
+  if (req.method === "GET" && req.url === "/ws") {
+    if (acceptable(req)) {
+      acceptWebSocket({
+        conn: req.conn,
+        bufReader: req.r,
+        bufWriter: req.w,
+        headers: req.headers,
+      }).then(chat);
+    }
+  }
+});
+console.log(`Server started on port ${port}`);
